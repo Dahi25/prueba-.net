5f5ecde9ba81323945288a947cbee082ab8514e5
Dahi25 <66143666+Dahi25@users.noreply.github.com>
2021-10-04 15:31:15 -0500

    Add files via upload
    Subiendo los archivos de conexion

---
+++ DATOS/DBConexion.cs
@@ -0,0 +1,28 @@
+﻿namespace ConexionDB
+{
+    public class DBConexion
+    {
+        //conexion a la Base de datos Compurent y Ecomerce 
+        public string GetConexion(string demoDB = "")
+        {
+            string cadena = string.Empty;
+            string database = "PRUEBA";
+            if (!string.IsNullOrWhiteSpace(demoDB))
+            {
+                database = demoDB;
+            }
+
+            try
+            {
+                cadena = @"Server=CRMV-FRONTDBOLD\DEMO; User id=sa; Password=ofima.sql10; multipleactiveresultsets=True; timeout=0; Initial Catalog=" + database;
+            }
+            catch
+            {
+            }
+            return cadena;
+        }
+    }
+}
+
+
+

a1948402cd33b3fddee611dbd5ad527e6dca1e91
Dahi25 <66143666+Dahi25@users.noreply.github.com>
2021-10-04 14:29:15 -0500

    Add files via upload
    subiendo el proyecto

---
+++ Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PruebaDahiana.Models
+{
+    public class Usuario
+    {
+        public String Cedula { get; set; }
+        public String Nombre { get; set; }
+        public String Cargo { get; set; }
+        public String Empresa { get; set; }
+
+    }
+}

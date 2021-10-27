353d0a0eb5e5cfa740de5d78a72eb9f24fbd71da
Dahi25 <66143666+Dahi25@users.noreply.github.com>
2021-10-27 15:53:48 -0500

    Update usuariosController.cs
    se agregaron vistas y  cambios en la bd

--- Controllers/usuariosController.cs
+++ Controllers/usuariosController.cs
@@ -23,117 +23,236 @@ namespace PruebaDahiana.Controllers
     }
 }
 
-@model PruebaDahiana.Models.Usuario
+using System;
+using System.Data.SqlClient;
+using System.Web.Mvc;
+using ConexionDB;
+using PruebaDahiana.Models;
 
-@{
-    ViewBag.Title = "Garantía";
-}
-<div style="width: 100%;">
-    <h1 class="text-white"> Solicita tus  garantías: </h1>
-</div>
-@using (Html.BeginForm("", "", FormMethod.Post, new { name = "formulario1", id = "formulario1" }))
+namespace PruebaDahiana.Controllers
 {
-    @Html.AntiForgeryToken()
+    public class UsuariosController : Controller
+    {
+        // GET: Usuarios
+        private static DBConexion DBConexion = new DBConexion();
+        private static Usuario user = new Usuario(); //para usar el objecto en varias vistas
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
 
-    <div class="form-horizontal">
+        public ActionResult Registro()
+        {
+            var user = new Usuario();
 
-        @Html.ValidationSummary(true, "", new { @class = "text-danger" })
-        <div class="d-flex">
-            <div>
-                <div class=" test">
-                    @Html.LabelFor(model => model.Cedula, htmlAttributes: new { @class = "test text-white" })
-                    <div class="">
-                        @Html.EditorFor(model => model.Cedula, new { htmlAttributes = new { @class = "form-control" } })
-                        @Html.ValidationMessageFor(model => model.Cedula, "", new { @class = "text-danger" })
-                    </div>
-                </div>
-                <div class=" test">
-                    @Html.TextAreaFor(model => model.Falla, htmlAttributes: new { @class = " test " })
 
-                    
-                    <div class="">
-                        @*  @Html.EditorFor(model => model.Falla, new { htmlAttributes = new { @class = "form-control" } })*@
-                        @Html.ValidationMessageFor(model => model.Falla, "", new { @class = "text-danger" })
-                    </div>
-                </div>
 
-            </div>
-            </div>
-            <div class="form-check  text-white">
-                <div>
-                    @Html.Label("Seleccione el servicio  que necesita garantía:")
-                    <div>
-                        <div class="d-flex  flex-column ">
-                            @Html.Label("Licenciamiento de office 365 ")
-                            @Html.CheckBoxFor(m => m.licenciamiento, new { id = "1" })
-                        </div>
-                        <div class="d-flex  flex-column">
-                            @Html.Label("Licenciamiento de Azure ")
-                            @Html.CheckBoxFor(m => m.azure, new { id = "2" })
-                        </div>
-                        <div class="d-flex  flex-column">
-                            @Html.Label("Renting tecnológico  ")
-                            @Html.CheckBoxFor(m => m.renting, new { id = "3" })
-                        </div>
+            return View(user);
 
-                    </div>
-
-                </div>
-            </div>
-        </div>
-        <div class="">
-            <input type="button" value="Enviar" onclick="EnviarFormulario();"  id="envio"   class="btn btn-warning" />
-        </div>
-}
-
-    <script>
-        $(document).ready(function () {
-            $('#formulario1').submit(function (e) {
-                e.preventDefault()
-            }).validate({
-                debug: false,
-                errorClass: "error nuevo",
-                rules: {
-                    Cedula: { required: true },
-                    Falla: { required: true }
-                     messages: {
-                        Cedula: {
-                            required: "Ingrese porfavor su cedula"
-                        },
-                        Falla: {
-                            required: "Describa las fallas que tiene su servicio"
-                         }
-                     }
-                }
-            });
-        });
-       
-    </script>
-
-<script>
-    function EnviarFormulario() {
-        debugger;
-
-        if ($("#formulario1").valid()) {
-            var formulario1 = $("#formulario1").serialize();
-
-            $.ajax({
-                type: 'POST',
-                url: '@Url.Action("EnviarSolicitud", "Usuarios")',
-                data: formulario1,
-                success: function (resultado) {
-                    swal({
-                        type: 'success',
-                        title: 'Estimado Usuario',
-                        text: 'Su solicitud ha sido enviada',
-                        confirmButtonColor: '#7E8A97'
-                    }).then(function () {
-                        location.reload();
-                    });
-                }
-            });
         }
+        public JsonResult RegistrarDatos(Usuario objUsuario) /* llamamos el metodo donde estamos guardando los datos*/
+        {
+
+            var resultado = false;
+
+            resultado = RegistrarUsuarios(objUsuario);
+            return Json(resultado, JsonRequestBehavior.AllowGet);
         }
 
-</script>
+
+        public bool RegistrarUsuarios(Usuario user)
+        {
+            bool result = false;
+
+
+            try
+            {
+
+                using (SqlConnection conexion = new SqlConnection(DBConexion.GetConexion()))
                 
+                {
+                    conexion.Open();
+            
+
+        var INSERT = @"INSERT INTO datos (Cedula,Nombre,Cargo,Empresa,licenciamiento,azure,renting,Opcion) VALUES(@pCedula,@pNombre,@pCargo,@pEmpresa,@plicenciamiento,@pazure,@prenting,@pOpcion)";
+
+                    using (SqlCommand comando = new SqlCommand(INSERT, conexion))
+                    {
+                        comando.Parameters.AddWithValue("@pCedula", user.Cedula);
+                        comando.Parameters.AddWithValue("@pNombre", user.Nombre);
+                        comando.Parameters.AddWithValue("@pCargo", user.Cargo);
+                        comando.Parameters.AddWithValue("@pEmpresa", user.Empresa);
+                        comando.Parameters.AddWithValue("@plicenciamiento", user.licenciamiento);
+                        comando.Parameters.AddWithValue("@pazure", user.azure);
+                        comando.Parameters.AddWithValue("@prenting", user.renting);
+                        comando.Parameters.AddWithValue("@pOpcion", user.Opcion);
+                        comando.ExecuteNonQuery();
+                    }
+
+
+                    result = true;
+                    conexion.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+        public JsonResult ActualizarDatos(Usuario objUsuario) /* llamamos el metodo donde estamos guardando los datos*/
+        {
+
+            var respuesta = false;
+
+            respuesta = ActualizarUsuarios(objUsuario);
+            return Json(respuesta, JsonRequestBehavior.AllowGet);
+        }
+
+        /* Metodo para actualizar datos */
+        public bool ActualizarUsuarios(Usuario user)
+        {
+            bool result = false;
+
+            try
+            {
+
+                using (SqlConnection conexion = new SqlConnection(DBConexion.GetConexion()))
+                {
+                    conexion.Open();
+                    var UPDATE = @"UPDATE datos SET Nombre = @pNombre, Cargo=@pCargo, Empresa=@pEmpresa,licenciamiento=@licenciamiento,azure=@pazure,renting=@prenting,Opcion=@Opcion where Cedula = @pCedula";
+
+
+
+
+
+
+                    using (SqlCommand comando = new SqlCommand(UPDATE, conexion))
+                    {
+                        comando.Parameters.AddWithValue("@pCedula", user.Cedula);
+                        comando.Parameters.AddWithValue("@pNombre", user.Nombre);
+                        comando.Parameters.AddWithValue("@pCargo", user.Cargo);
+                        comando.Parameters.AddWithValue("@pEmpresa", user.Empresa);
+                        comando.Parameters.AddWithValue("@plicenciamiento", user.licenciamiento);
+                        comando.Parameters.AddWithValue("@pazure", user.azure);
+                        comando.Parameters.AddWithValue("@prenting", user.renting);
+                        comando.ExecuteNonQuery();
+
+                    }
+
+
+                    result = true;
+                    conexion.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+        public JsonResult EliminarDatos(Usuario objUsuario) /* llamamos el metodo donde estamos guardando los datos*/
+        {
+
+            var respuesta = false;
+
+            respuesta = EliminarUsuarios(objUsuario);
+            return Json(respuesta, JsonRequestBehavior.AllowGet);
+        }
+
+        /* Metodo para Eliminar datos */
+        public bool EliminarUsuarios(Usuario user)
+        {
+            bool result = false;
+
+            try
+            {
+
+                using (SqlConnection conexion = new SqlConnection(DBConexion.GetConexion()))
+                {
+                    conexion.Open();
+                    var DElETE = @"DElETE from datos  where Cedula = @pCedula";
+
+
+
+
+
+
+                    using (SqlCommand comando = new SqlCommand(DElETE, conexion))
+                    {
+                        comando.Parameters.AddWithValue("@pCedula", user.Cedula);
+                        comando.ExecuteNonQuery();
+
+                    }
+
+
+                    result = true;
+                    conexion.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+        public ActionResult Garantía()
+        {
+            var user = new Usuario();
+
+
+
+            return View(user);
+
+        }
+        public JsonResult EnviarSolicitud(Usuario objUsuario) /* llamamos el metodo donde estamos guardando los datos*/
+        {
+
+            var respuestaone = false;
+
+            respuestaone = EnviarUsuarios(objUsuario);
+            return Json(respuestaone, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public bool EnviarUsuarios(Usuario user)
+        {
+            bool result = false;
+
+            try
+            {
+
+                using (SqlConnection conexion = new SqlConnection(DBConexion.GetConexion()))
+                {
+                    conexion.Open();
+
+                    var INSERT = @"INSERT INTO garantias (Cedula,Falla,licenciamiento,azure,renting) VALUES(@pCedula,@pFalla,@plicenciamiento,@pazure,@prenting)";
+
+                    using (SqlCommand comando = new SqlCommand(INSERT, conexion))
+                    {
+                        comando.Parameters.AddWithValue("@pCedula", user.Cedula);
+                        comando.Parameters.AddWithValue("@pFalla", user.Falla);
+                        comando.Parameters.AddWithValue("@plicenciamiento", user.licenciamiento);
+                        comando.Parameters.AddWithValue("@pazure", user.azure);
+                        comando.Parameters.AddWithValue("@prenting", user.renting);
+                        comando.ExecuteNonQuery();
+                    }
+
+
+                    result = true;
+                    conexion.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+    }
+    
+  }

9daf22e318153d783e5e2ae38546df6e8d168dc5
Dahi25 <66143666+Dahi25@users.noreply.github.com>
2021-12-13 15:52:24 -0600

    Add files via upload
    se agregaron funciones nuevas

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿
 using System.Web.Mvc;
 
-namespace PruebaDahiana.Controllers
+namespace Prueba.Controllers
 {
     public class HomeController : Controller
     {

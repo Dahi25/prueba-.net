aeffeb4a4d002a572c566274d091afd29324144a
Dahi25 <66143666+Dahi25@users.noreply.github.com>
2021-12-13 15:40:30 -0600

    Add files via upload
    se creo una conexión nueva

---
+++ datos1/conexionDB.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace conexionDB
+{
+    public class conexionDB1
+    {
+        //Conexión a la Base de datos Compurent y eCommerce 
+        public string GetConexion(string demoDB = "")
+        {
+            string cadena = string.Empty;
+            string database = "USUARIO";
+            if (!string.IsNullOrWhiteSpace(demoDB))
+            {
+                database = demoDB;
+            }
+
+            try
+            {
+                cadena = @"Server=CRMV-FRONTDBOLD\DEMO; User id=sa; Password=ofima.sql10; multipleactiveresultsets=True; timeout=0; Initial Catalog=" + database;
+            }
+            catch
+            {
+            }
+            return cadena;
+        }
+    }
+}
